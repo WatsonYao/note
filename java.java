e4a2446982e18c20131a06e864b9e54d49a2cc5f
WatsonYao <yaoyuan131617@gmail.com>
2014-12-10 22:11:46 +0800

    关于View
    自定义view的细节

--- java.java
+++ java.java
@@ -116,3 +116,178 @@ public class Client{
 
 // 工厂方法模式之所以有一个别名叫 多态性工厂模式
 // 具体工厂类都有共同的接口，或者都有共同的抽象父类
+
+
+//View相关
+public class SimpleLayout extends ViewGroup{
+
+	public SimpleLayout(Context context, AttributeSet attrs){
+		super(context,attrs);
+	}
+
+	protected void onMeasure(int widthMeasureSper, int heightMeasureSpec){
+		super.onMeaser(widthMeasureSper,heightMeasureSpec);
+
+		//如果有子视图，就调用measureChild()来测量出子视图的大小
+		if(getChildCount() > 0){
+			View childView = getChildAt(0);
+			measureChild(childView,widthMeasureSper,heightMeasureSpec);
+		}
+	}
+
+	protected void onLayout(boolean changed,int l, int t, int r, int b){
+		if(getChildCount() > 0){
+			View childView = getChildAt(0);
+			childView.layout(0,0,childView.getMeasureWidth(),childView.getMeasureHeight());
+		}
+	}
+}
+
+// View你们的canvas
+public class MyView extends View implements OnClickListener{
+	private Paint mPaint;
+
+	public MyView(Context context, AttributeSet attrs){
+		super(context,attrs);
+		mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+	}
+
+	public void onDraw(Canvas canvs){
+		mPaint.setColor(Color.YELLOW);
+		canvas.drawRect(0,0,getWidth(),getHeight(),mPaint);
+
+		mPaint.setColor(Color.Blue);
+		mPaint.setTextSize(20);
+		String text = "Hello View";
+		canvas.drawText(text,0,getHeight()/2, mPaint);
+	}
+
+	@Override  
+    public void onClick(View v) {  
+         
+        invalidate();  
+    } 
+}
+
+//首先我们在CounterView的构造函数中初始化了一些数据，
+//并给这个View的本身注册了点击事件，这样当CounterView被点击的时候，onClick()方法就会得到调用。
+
+//组合控件
+public class TitleView extends FrameLayout{
+
+	private Button leftButton;
+	private TextView titleText;
+
+	public TitleView(Context context, AttributeSet attrs){
+		super(context, attrs);
+
+		LayoutInflater.from(context).inflate(R.layout.title, this);
+
+		titleText = (TextView)findViewById(R.id.title_text);
+		leftButton = (Button) findViewById(R.id.button_left);
+
+		leftButton.setOnClickListenser(new OnClickListener(){
+			public void onClick(View v){
+				((Activity)getContext()).finish();
+			}
+		});
+	}
+
+	public void setTitleText(String text){
+		titleText.setText(text);
+	}
+
+	public void setLeftButtonText(String text){
+		leftButton.setText(text);
+	}
+
+	public void setLeftButtonListener(OnClickListener l){
+		leftButton.setOnClickListenser(l);
+	}
+}
+
+//继承控件
+public class MyListView extends ListView implements OnTouchListener,OnGestureLisener{
+	
+	private GestureDetetor gestureDector;
+	private onDeleteListener listener;
+	private View deleteButton;
+	private ViewGroup itemLayout;
+	private int selectedItem;
+	private boolean isDeleteShown;
+
+	public MyListView(Context context,AttributeSet attrs){
+		super(context, attrs);
+
+		gestureDector = new GestureDetetor(getContext(),this);
+		setOnTouchListener(this);
+	}
+
+	public void setOnDeleteListener(onDeleteListener l){
+		listener = l;
+	}
+
+	public boolean onTouch(View v, MotionEvent event){
+		if(isDeleteShown){
+			itemLayout.removeView(deleteButton);
+			deleteButton = null;
+			isDeleteShown = false;
+			return false;
+		}else{
+			return gestureDector.onTouchEvent(event);
+		}
+	}
+
+	public boolean onDown(MotionEvent e){
+		if(!isDeleteShown){
+			selectedItem = pointToPosition((int)e.getX(),(int)e.getY());
+		}
+		return false;
+	}
+
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY){
+		if(!isDeleteShown && Math.abs(velocityX) > Math.abs(velocityY)){
+			deleteButton = LayoutInflater.from(getContext().inflate(R.layout.delete_button,null));
+			deleteButton.setOnClickListener(new OnClickListener(){
+				public void onClick(View v){
+					itemLayout.removeView(deleteButton);
+					deleteButton = null;
+					isDeleteShown = false;
+					listener.onDelete(selectedItem);
+				}
+			});
+
+			itemLayout = (ViewGroup)getChildAt(selectedItem - getFirstVisibilePosition());
+
+			RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+			params.addRule(RelativeLayout.ALIGN_PARAENT_RIGHT);
+			params.addRule(RelativeLayout.CENTER_VERTICAL);
+
+			itemLayout.addView(deleteButton,params);
+			isDeleteShown = true;
+		}
+
+		return false;
+	}
+
+	public boolean onSingleTapUp(MotionEvent e){
+		return false;
+	}
+
+	public void onShowPress(MotionEvent e){
+
+	}
+
+	public boolean onScroll(MotionEvent e1, MotionEvent e2,float distanceX, float distanceY){
+		return false;
+	}
+
+	public void onLongPress(MotionEvent e){
+
+	}
+
+	public interface OnDeleteListener{
+		void onDelete(int index);
+	}
+}
