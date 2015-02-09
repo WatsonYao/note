c892c0c1c37e939d131c1469bdfbfd96074728c3
WatsonYao <yaoyuan131617@gmail.com>
2015-02-09 17:49:26 +0800

    google sample 多点触摸基本
    还有静态内部类

--- java.java
+++ java.java
@@ -4490,6 +4490,328 @@ public class MainActivity extends Activity implements OnClickListener,IUserView{
 }
 
 
+// google sample 多点触摸基本
+public final class Pools{
+	public static interface Pool<T>{
+		public T acquire();
+		public boolean release(T insatnce);
+	}
+
+	private Pools(){
+
+	}
+
+	public static class SimplePool<T> implements Pool<T>{
+		private final Object[] mPool;
+		private int mPoolSize;
+
+		public SimplePool(int maxPoolSize){
+			if(maxPoolSize <= 0){
+				throw new IllegalArgumentException("xxx");
+			}
+			mPool = new Object[maxPoolSize];
+		}
+
+		public T acquire(){
+			if(mPoolSize>0){
+				final int lastPooledIndex = mPoolSize - 1;
+				T instance = (T) mPool[lastPooledIndex];
+				mPool[lastPooledIndex] = null;
+				mPoolSize--;
+				return instance;
+			}
+			return null;
+		}
+
+		public boolean release(T instance){
+			if(isInPool(instance)){
+				throw new IllegalStateException("xxx");
+			}
+			if(mPoolSize < mPool.length){
+				mPool[mPoolSize] = instance;
+				mPoolSize++;
+				return true;
+			}
+			return false;
+		}
+
+		private boolean isInPool(T instance){
+			for(int i=0; i<mPoolSize; i++){
+				return true;
+			}
+			return false;
+		}
+	}
+
+	public static class SynchronizedPool<T> extends SimplePool<T>{
+		public SynchronizedPool(int maxPoolSize){
+			super(maxPoolSize);
+		}
+
+		public T acquire(){
+			synchronized(mLock){
+				return super.acquire();
+			}
+		}
+
+		public boolean release(T element){
+			synchronized(mLock){
+				return super.release(element);
+			}
+		}
+	}
+}
+
+// 内部类
+// 静态内部类 static nested class 封装性和可读性
+// 内部类	inner class
+// 而静态内部类，则只可以访问外部类的静态方法和静态属性
+//（如果是private权限也能访问，这是由其代码位置所决定的），其他则不能访问。
+// 而静态内部类是可以独立存在的，即使外部类消亡了，静态内部类还是可以存在的。
+// 普通内部类不能声明static的方法和变量，
+// 注意这里说的是变量，常量（也就是final static修饰的属性）还是可以的，而静态内部类形似外部类，没有任何限制。
+public class TouchDisplayView extends View{
+
+	private SparseArray<TouchHistory> mTouches;
+	private boolean mHasTouch = false;
+
+	static final class TouchHistory{
+		public static final int HISTORY_COUNT = 20;
+
+		public float x;
+		public float y;
+		public float pressure = 0f;
+		public String label = null;
+
+		public int historyIndex = 0;
+		public int historyCount = 0;
+
+		public PointF[] history = new PoinitF[HISTORY_COUNT];
+
+		private static final int MAX_POOL_SIZE = 10;
+		private static final SimplePool<TouchHistory> sPool 
+			= new SimplePool<TouchHistory>(MAX_POOL_SIZE);
+
+		public static TouchHistory obtain(float x, float y, float pressure){
+			TouchHistory data = sPool.acquire();
+			if(data == null){
+				data = new TouchHistory();
+			}
+			data.setTouch(x,y,pressure);
+			return data;
+		}
+
+		public TouchHistory(){
+			for( int i=0; i<HISTORY_COUNT; i++){
+				history[i] = new PointF();
+			}
+		}
+
+		public void setTouch(float x, float y, float pressure){
+			this.x = x;
+			this.y = y;
+			this.pressure = pressure;
+		}
+
+		public void recycle(){
+			this.historyIndex = 0;
+			this.historyCount = 0;
+			sPool.release(this);
+		}
+
+		public void addHistory(float x, float y){
+			PointF p = history[historyIndex];
+			p.x = x;
+			p.y = y;
+
+			historyIndex = (historyIndex + 1)% history.length;
+
+			if(historyCount < HISTORY_COUNT){
+				historyCount++;
+			}
+		}
+	}
+
+	public TouchDisplayView(Context context, AttributeSet attrs){
+		super(context, attrs);
+
+		mTouches = new SparseArray<TouchHistory>(10);
+
+		initialisePaint();
+	}
+
+	@Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        final int action = event.getAction();
+
+        /*
+         * Switch on the action. The action is extracted from the event by
+         * applying the MotionEvent.ACTION_MASK. Alternatively a call to
+         * event.getActionMasked() would yield in the action as well.
+         */
+        switch (action & MotionEvent.ACTION_MASK) {
+
+            case MotionEvent.ACTION_DOWN: {
+                // first pressed gesture has started
+
+                /*
+                 * Only one touch event is stored in the MotionEvent. Extract
+                 * the pointer identifier of this touch from the first index
+                 * within the MotionEvent object.
+                 */
+                int id = event.getPointerId(0);
+                Log.i("temp","ACTION_DOWN-id->" + id);
+
+                TouchHistory data = TouchHistory.obtain(event.getX(0), event.getY(0),
+                        event.getPressure(0));
+                data.label = "id: " + 0;
+
+
+                /*
+                 * Store the data under its pointer identifier. The pointer
+                 * number stays consistent for the duration of a gesture,
+                 * accounting for other pointers going up or down.
+                 */
+                mTouches.put(id, data);
+
+                mHasTouch = true;
+
+                break;
+            }
+
+            case MotionEvent.ACTION_POINTER_DOWN: {
+                /*
+                 * A non-primary pointer has gone down, after an event for the
+                 * primary pointer (ACTION_DOWN) has already been received.
+                 */
+
+                /*
+                 * The MotionEvent object contains multiple pointers. Need to
+                 * extract the index at which the data for this particular event
+                 * is stored.
+                 */
+                int index = event.getActionIndex();
+                int id = event.getPointerId(index);
+                Log.i("temp","ACTION_POINTER_DOWN-id->" + id + ",index->" + index);
+
+                TouchHistory data = TouchHistory.obtain(event.getX(index), event.getY(index),
+                        event.getPressure(index));
+                data.label = "id: " + id;
+
+                /*
+                 * Store the data under its pointer identifier. The index of
+                 * this pointer can change over multiple events, but this
+                 * pointer is always identified by the same identifier for this
+                 * active gesture.
+                 */
+                mTouches.put(id, data);
+
+                break;
+            }
+
+            case MotionEvent.ACTION_UP: {
+                /*
+                 * Final pointer has gone up and has ended the last pressed
+                 * gesture.
+                 */
+
+                /*
+                 * Extract the pointer identifier for the only event stored in
+                 * the MotionEvent object and remove it from the list of active
+                 * touches.
+                 */
+                int id = event.getPointerId(0);
+                Log.i("temp","ACTION_UP-id->" + id);
+                TouchHistory data = mTouches.get(id);
+                mTouches.remove(id);
+                data.recycle();
+
+                mHasTouch = false;
+
+                break;
+            }
+
+            case MotionEvent.ACTION_POINTER_UP: {
+                /*
+                 * A non-primary pointer has gone up and other pointers are
+                 * still active.
+                 */
+
+                /*
+                 * The MotionEvent object contains multiple pointers. Need to
+                 * extract the index at which the data for this particular event
+                 * is stored.
+                 */
+                int index = event.getActionIndex();
+                int id = event.getPointerId(index);
+                Log.i("temp","ACTION_POINTER_DOWN-id->" + id + ",index->" + index);
+                TouchHistory data = mTouches.get(id);
+                mTouches.remove(id);
+                data.recycle();
+
+                break;
+            }
+
+            case MotionEvent.ACTION_MOVE: {
+                /*
+                 * A change event happened during a pressed gesture. (Between
+                 * ACTION_DOWN and ACTION_UP or ACTION_POINTER_DOWN and
+                 * ACTION_POINTER_UP)
+                 */
+
+                /*
+                 * Loop through all active pointers contained within this event.
+                 * Data for each pointer is stored in a MotionEvent at an index
+                 * (starting from 0 up to the number of active pointers). This
+                 * loop goes through each of these active pointers, extracts its
+                 * data (position and pressure) and updates its stored data. A
+                 * pointer is identified by its pointer number which stays
+                 * constant across touch events as long as it remains active.
+                 * This identifier is used to keep track of a pointer across
+                 * events.
+                 */
+                Log.i("temp","ACTION_MOVE-PointerCount->" + event.getPointerCount());
+                for (int index = 0; index < event.getPointerCount(); index++) {
+                    // get pointer id for data stored at this index
+                    int id = event.getPointerId(index);
+
+                    // get the data stored externally about this pointer.
+                    TouchHistory data = mTouches.get(id);
+
+                    // add previous position to history and add new values
+                    data.addHistory(data.x, data.y);
+                    data.setTouch(event.getX(index), event.getY(index),
+                            event.getPressure(index));
+
+                }
+
+                break;
+            }
+        }
+
+        // trigger redraw on UI thread
+        this.postInvalidate();
+
+        return true;
+    }
+
+
+	protected void onDraw(Canvas canvas){
+		if(mHasTouch){
+			canvas.drawColor();
+		}else{
+			canvas.drawRect();
+		}
+
+		for(int i=0; i< mTouches.size(); i++){
+			int id = mTouches.keyAt(i);
+			TouchHistory data = mTouches.valueAt(i);
+			drawCircle(canvas, id, data);
+		}
+	}
+}
+
 
 
 
