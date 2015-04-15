8a456c8ac0366f440fd1e7df05b9ec9b7c59b3dd
WatsonYao <yaoyuan131617@gmail.com>
2015-04-15 12:05:05 +0800

    handle使用方法
    handle使用方法

--- java.java
+++ java.java
@@ -8852,6 +8852,52 @@ public class MainActivity extends Activity
     }  
 }  
 
+// handle的使用方法
+
+public class SampleActivity extends Activity {
+
+  /**
+   * Instances of static inner classes do not hold an implicit
+   * reference to their outer class.
+   */
+  private static class MyHandler extends Handler {
+    private final WeakReference<SampleActivity> mActivity;
+
+    public MyHandler(SampleActivity activity) {
+      mActivity = new WeakReference<SampleActivity>(activity);
+    }
+
+    @Override
+    public void handleMessage(Message msg) {
+      SampleActivity activity = mActivity.get();
+      if (activity != null) {
+        // ...
+      }
+    }
+  }
+
+  private final MyHandler mHandler = new MyHandler(this);
+
+  /**
+   * Instances of anonymous classes do not hold an implicit
+   * reference to their outer class when they are "static".
+   */
+  private static final Runnable sRunnable = new Runnable() {
+      @Override
+      public void run() { /* ... */ }
+  };
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Post a message and delay its execution for 10 minutes.
+    mHandler.postDelayed(sRunnable, 1000 * 60 * 10);
+
+    // Go back to the previous Activity.
+    finish();
+  }
+}
 
 
 
